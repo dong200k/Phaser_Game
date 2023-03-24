5fa94434bd895fc543928f75df4d76b1e231347d
Sheng Wei Zhu <shengwei.zhu@stonybrook.edu>
2023-03-24 01:12:35 -0400

    fixed issue where tsx Tiled file confilcts with webpack

--- client/dist/tilemaps/demo_map/dirt_dungeon_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="dirt_dungeon_tileset" tilewidth="16" tileheight="16" tilecount="180" columns="20">
- <image source="dirt_dungeon_tileset.png" width="320" height="144"/>
-</tileset>
