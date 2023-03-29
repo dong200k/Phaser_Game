4eec140646087abf5fb9e696b6290071dff708bc
dong200k <dchen5678@gmail.com>
2023-03-29 17:02:01 -0400

    reverted to previous commit

---
+++ server/src/rooms/game_room/schemas/gameobjs/Weapons/BaseWeapon.ts
@@ -0,0 +1,15 @@
+import { Schema, type } from '@colyseus/schema';
+import Stat from '../Stat';
+
+export default class BaseWeapon extends Schema {
+    @type(Stat) stat
+    @type("string") name
+    @type("string") description
+
+    constructor(name: string, description: string) {
+        super(0, 0);
+        this.stat = new Stat()
+        this.name = name
+        this.description = description
+    }
+}
