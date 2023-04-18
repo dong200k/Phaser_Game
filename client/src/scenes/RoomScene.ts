54a091b6c69ccd2d189e997929a58818421645b7
Sheng Wei Zhu <shengwei.zhu@stonybrook.edu>
2023-04-17 23:55:28 -0400

    Added basic UI for RoomScene

--- client/src/scenes/RoomScene.ts
+++ client/src/scenes/RoomScene.ts
@@ -2,8 +2,11 @@ import Phaser from "phaser";
 import * as Colyseus from 'colyseus.js';
 import ClientManager from "../system/ClientManager";
 import NavButton from "../UI/NavButton";
-import { SceneKey } from "../config";
+import { ColorStyle, SceneKey } from "../config";
 import Button from "../UI/Button";
+import TextBox from "../UI/TextBox";
+import Layout from "../UI/Layout";
+import SceneManager from "../system/SceneManager";
 
 export default class RoomScene extends Phaser.Scene {
     
@@ -12,6 +15,7 @@ export default class RoomScene extends Phaser.Scene {
 
     private playersInRoomText?: Phaser.GameObjects.Text;
     private playersInRoom: number = 0;
+    private roomIDText: TextBox | null = null;
 
     constructor() {
         super(SceneKey.RoomScene);
@@ -25,8 +29,8 @@ export default class RoomScene extends Phaser.Scene {
     }
 
     private initializeUI() {
-        let rect = {x: this.game.scale.width / 2, y: 50, width: 200, height: 50, color: 0xAAAAAA}
-        let textPos = {x: this.game.scale.width / 2 - 48, y: 42}
+        // let rect = {x: this.game.scale.width / 2, y: 50, width: 200, height: 50, color: 0xAAAAAA}
+        // let textPos = {x: this.game.scale.width / 2 - 48, y: 42}
 
         // // leave to lobby button
         // NavButton(this, "Join Lobby", () => {
@@ -39,14 +43,37 @@ export default class RoomScene extends Phaser.Scene {
         //     this.waitingRoom?.send('start')
         // }, {...textPos, y: 142}, {...rect, y: 150})
 
+        // ---------- Room ID -----------
+        this.roomIDText = new TextBox(this, "", "p4", ColorStyle.neutrals[900]);
+        this.roomIDText.setPosition(24, 24);
+        this.roomIDText.setOrigin(0, 0);
+        this.add.existing(this.roomIDText);
+
+        // --------- Leave Room Button ----------
+        let leaveButton = new Button(this, "Leave room", 0, 0, "regular", () => SceneManager.getSceneManager().popScene());
+        let leaveButtonLayout = new Layout(this, {originX: 0, originY: 1, x: 48, y: this.game.scale.height - 48});
+        leaveButtonLayout.add(leaveButton);
+        this.add.existing(leaveButtonLayout);
+
+        // --------- Other Buttons ------------
+        let selectRoleButton = new Button(this, "Select role", 0, 0, "regular", () => {console.log("select role button onclick")});
+        let selectDungeonButton = new Button(this, "Select dungeon", 0, 0, "regular", () => {console.log("select dungeon onclick")});
         let startButton = new Button(this, "Start", 0, 0, "large", () => {
             this.waitingRoom?.send('start');
-        })
+        });
 
-        this.add.existing(startButton);
+        let otherButtonLayout = new Layout(this, {
+            flexDirection: 'row', 
+            alignItems: 'center', 
+            gap: 100,
+            x: this.game.scale.width / 2,
+            y: this.game.scale.height / 2 + 200,
+        });
+        otherButtonLayout.add([selectRoleButton, startButton, selectDungeonButton]);
+        this.add.existing(otherButtonLayout);
 
         // list of players text
-        this.playersInRoomText = this.add.text(this.game.scale.width / 2, 300, "Players in room: 0");
+        this.playersInRoomText = this.add.text(this.game.scale.width / 2 - 50, 100, "Players in room: 0");
     }
 
     private updatePlayersInRoom(count: number) {
@@ -57,6 +84,7 @@ export default class RoomScene extends Phaser.Scene {
         ClientManager.getClient().joinWaitingRoom().then((room) => {
             this.waitingRoom = room;
             console.log("Joined waiting room");
+            this.roomIDText?.setText(`Room ID: ${room.id}`);
             this.onJoin();
         }).catch(e => {
             console.log("Failed to join waiting room ", e);
