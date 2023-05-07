4d61f9bc3606f1bea8654a4edb5fcd89ab2358d9
Sheng Wei Zhu <shengwei.zhu@stonybrook.edu>
2023-05-07 15:13:56 -0400

    Added ClientSidePrediction template

---
+++ client/src/system/ClientSidePrediction.ts
@@ -0,0 +1,13 @@
+
+/**
+ * The ClientSidePrediction class will manage the prediction of player1 to match that of the server's.
+ * Its main purpose would be movement prediction. Which would consist of the following:
+ *  - Movement from WASD
+ *  - Movement blocked by obstacles
+ *  - Animations from movement
+ * 
+ * To accomplish this updates on both the client and server would need to be deterministic.
+ */
+export default class ClientSidePrediction {
+
+}
