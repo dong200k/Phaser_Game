55d4bbb72328ba753fac49ceed7d03f9c242e415
dong200k <dchen5678@gmail.com>
2023-07-03 10:53:17 -0400

    added attack/mattack tests

--- server/src/rooms/game_room/system/Formulas/formulas.ts
+++ server/src/rooms/game_room/system/Formulas/formulas.ts
@@ -1,16 +1,16 @@
 import Stat from "../../schemas/gameobjs/Stat";
 
-const BASE_SPEED = 1
-const ARMOR_PEN_CAP = 0.3
-const MAGIC_PEN_CAP = 0.3
-const ATTACK_SPEED_CAP = 5
+export const BASE_SPEED = 1
+export const ARMOR_PEN_CAP = 0.3
+export const MAGIC_PEN_CAP = 0.3
+export const ATTACK_SPEED_CAP = 5
 
 /**
  * 
  * @param stat 
  * @returns stat's speed multiplied by base speed
  */
-const getFinalSpeed = function({speed}: Stat){
+export const getFinalSpeed = function({speed}: Stat){
     return speed * BASE_SPEED;
 }
 
@@ -18,15 +18,19 @@ const getFinalSpeed = function({speed}: Stat){
  * Takes in an attacker stat, a defender stack, and a attack damage multiplier, and returns the true attack damage the defender should take
  * @param attackerStat stat of attacker/projectile
  * @param defenderStat stat of defender/entity
- * @param attackMultiplier multiplier for the ad damage before armor is taken into account
+ * @param attackMultiplier non negative multiplier for the ad damage before armor is taken into account
  * @returns 
  */
-const getTrueAttackDamage = function({attack, attackPercent, damagePercent, critDamage, critRate, armorPen}: Stat, {armor}: Stat, attackMultiplier: number){
+export const getTrueAttackDamage = function({attack, attackPercent, damagePercent, critDamage, critRate, armorPen}: Stat, {armor}: Stat, attackMultiplier: number){
     const isCrit = Math.random() < critRate? 1 : 0;
     let damage = (attack * (1 + attackPercent + damagePercent)) * attackMultiplier
     if(isCrit) damage *= (critDamage + 1)
 
-    const effectiveArmor = armor * (1 - Math.min(armorPen, ARMOR_PEN_CAP))
+    let reductionMultiplier = (1 - Math.min(armorPen, ARMOR_PEN_CAP))
+    if(reductionMultiplier < 0) reductionMultiplier = 0
+    if(armor < 0) reductionMultiplier = 1 // Don't apply armor pen when armor is negative
+    let effectiveArmor = armor * reductionMultiplier
+
     const trueDamage = damage - effectiveArmor;
     return trueDamage < 0? 0 : trueDamage
 }
@@ -36,13 +40,18 @@ const getTrueAttackDamage = function({attack, attackPercent, damagePercent, crit
  * Takes in an attacker stat, defender stat, and a magic damage multiplier and returns the true magic damage the defender should take
  * @param attackerStat stat of attacker/projectile
  * @param defenderStat stat of defender/entity 
- * @param magicMultiplier multiplier for the ap damage before magic resist is taken into account
+ * @param magicMultiplier non negative multiplier for the ap damage before magic resist is taken into account
  * @returns 
  */
-const getTrueMagicDamage = function({magicAttack, magicAttackPercent, damagePercent, magicPen}: Stat, {magicResist}: Stat, magicMultiplier: number){
+export const getTrueMagicDamage = function({magicAttack, magicAttackPercent, damagePercent, magicPen}: Stat, {magicResist}: Stat, magicMultiplier: number){
     const damage = magicMultiplier * (magicAttack * (1 + magicAttackPercent + damagePercent))
-    const effectiveArmor = magicResist * (1 - Math.min(magicPen, MAGIC_PEN_CAP))
-    const trueDamage = damage - effectiveArmor;
+
+    let reductionMultiplier = (1 - Math.min(magicPen, MAGIC_PEN_CAP))
+    if(reductionMultiplier < 0) reductionMultiplier = 0
+    if(magicResist < 0) reductionMultiplier = 1 // negative magic res, dont apply magic pen
+    const effectiveMagicResist = magicResist * reductionMultiplier
+    
+    const trueDamage = damage - effectiveMagicResist;
     return trueDamage < 0? 0 : trueDamage
 }
 
@@ -52,7 +61,7 @@ const getTrueMagicDamage = function({magicAttack, magicAttackPercent, damagePerc
  * @param lifeSteal life steal percent
  * @returns total life steal flat
  */
-const getFinalLifeSteal = function(trueDamage: number, lifeSteal: number){
+export const getFinalLifeSteal = function(trueDamage: number, lifeSteal: number){
     return lifeSteal * trueDamage;
 }
 
@@ -62,8 +71,11 @@ const getFinalLifeSteal = function(trueDamage: number, lifeSteal: number){
  * @param BASE_ATTACK_COOLDOWN cooldown
  * @returns actual cooldown after accounting attack speed
  */
-const getFinalAttackCooldown = function({attackSpeed, attackSpeedPercent}: Stat, BASE_ATTACK_COOLDOWN: number){
+export const getFinalAttackCooldown = function({attackSpeed, attackSpeedPercent}: Stat, BASE_ATTACK_COOLDOWN: number){
     const totalAttackSpeed = attackSpeed * (1 + attackSpeedPercent)
+
+    if(totalAttackSpeed < 0 || totalAttackSpeed  === 0) return BASE_ATTACK_COOLDOWN
+
     const speedFactor = 1 / Math.min(totalAttackSpeed, ATTACK_SPEED_CAP)
     return BASE_ATTACK_COOLDOWN * speedFactor;
 }
@@ -72,12 +84,8 @@ const getFinalAttackCooldown = function({attackSpeed, attackSpeedPercent}: Stat,
 /**
  * 
  * @param stat 
- * @returns att
+ * @returns attack range
  */
-const getFinalAttackRange = function({attackRangePercent, attackRange}: Stat){
+export const getFinalAttackRange = function({attackRangePercent, attackRange}: Stat){
     return (1 + attackRangePercent) + attackRange;
-}
-
-export {
-    getFinalAttackCooldown, getTrueAttackDamage, getFinalAttackRange, getFinalLifeSteal, getTrueMagicDamage, getFinalSpeed
 }
--- server/src/rooms/game_room/system/Formulas/tests/attack.test.ts
+++ server/src/rooms/game_room/system/Formulas/tests/attack.test.ts
@@ -1,5 +1,5 @@
 import Stat from "../../../schemas/gameobjs/Stat"
-import { getTrueAttackDamage } from "../formulas"
+import { ARMOR_PEN_CAP, getTrueAttackDamage } from "../formulas"
 
 describe("Attack Formula Test", ()=>{
 
@@ -41,17 +41,110 @@ describe("Attack Formula Test", ()=>{
         damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
         expect(damage).toBe(0)
 
+        // -10 armor
+        defenderStat.armor = -10
+        damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+        expect(damage).toBe(20)
+
     })
     test("Attacker Attack penetration test", ()=>{
+        function test(armorPen: number){
+            let attackMultiplier = 1
+            attackerStat.attack = 10
+            attackerStat.armorPen = armorPen
+    
+            // zero armor
+            defenderStat.armor = 0
+            let damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+            expect(damage).toBe(10)
+    
+            // 10 armor
+            defenderStat.armor = 10
+            damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+            let expectedArmorAfterPenetration = 10 * (1 - Math.min(ARMOR_PEN_CAP, armorPen))
+            let expectedDamage = 10 - expectedArmorAfterPenetration
+            expect(damage).toBe(expectedDamage)
+    
+            // -10 armor so armor pen is not applied
+            defenderStat.armor = -10
+            damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+            expect(damage).toBe(20)
+        }
         
+        test(0) // 0% armor pen
+        test(0.3) // 30% armor pen which is the ARMOR_PEN_CAP
+        test(0.5) // 50% armor pen which is > ARMOR_PEN_CAP
     })
     test("Attack Multiplier Test", ()=>{
-
+        function test(attackMultiplier: number){
+            attackerStat.attack = 10
+    
+            // zero armor
+            defenderStat.armor = 0
+            let damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+            let expectedDamage = 10 * attackMultiplier
+            if(expectedDamage < 0) expectedDamage = 0
+            expect(damage).toBe(expectedDamage)
+    
+            // 10 armor
+            defenderStat.armor = 10
+            damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+            expectedDamage = 10 * attackMultiplier - defenderStat.armor
+            if(expectedDamage < 0) expectedDamage = 0
+            expect(damage).toBe(expectedDamage)
+    
+            // -10 armor so armor pen is not applied
+            defenderStat.armor = -10
+            damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+            expectedDamage = 10 * attackMultiplier - defenderStat.armor
+            if(expectedDamage < 0) expectedDamage = 0
+            expect(damage).toBe(expectedDamage)
+        }
+        
+        test(0) // 0% attack multiplier
+        test(0.5) // 50% attack multiplier
+        test(1) // 100%
+        test(10) // 1000%
     })
     test("Attacker with zero attack stats", ()=>{
+        // zero armor
+        let attackMultiplier = 1
+        defenderStat.armor = 0
+        let damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+        expect(damage).toBe(0)
 
+        // 10 armor
+        defenderStat.armor = 10
+        damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+        expect(damage).toBe(0)
+
+        // -10 armor so armor pen is not applied
+        defenderStat.armor = -10
+        damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+        expect(damage).toBe(10)
     })
     test("Attacker with non zero attack stats", ()=>{
+        attackerStat.attack = 10
+        attackerStat.armorPen = 0.3
+        attackerStat.critRate = 1
+        attackerStat.critDamage = 1
+        attackerStat.damagePercent = 1
+        attackerStat.attackPercent = 1
 
+        // zero armor
+        let attackMultiplier = 1
+        defenderStat.armor = 0
+        let damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+        expect(damage).toBe(60)
+
+        // 10 armor
+        defenderStat.armor = 10
+        damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+        expect(damage).toBe(53)
+
+        // -10 armor so armor pen is not applied
+        defenderStat.armor = -10
+        damage = getTrueAttackDamage(attackerStat, defenderStat, attackMultiplier)
+        expect(damage).toBe(70)
     })
 })
