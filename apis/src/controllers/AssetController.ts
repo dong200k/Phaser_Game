b7c8386713c1248268c74b3095024d503cedcc5c
Sheng Wei Zhu <shengwei321@gmail.com>
2023-11-05 11:19:10 -0500

    Added host option to firebase.json

--- apis/src/controllers/AssetController.ts
+++ apis/src/controllers/AssetController.ts
@@ -165,10 +165,11 @@ export default class AssetController {
      */
     public static restoreAsset(req: any, res: any) {
         let {loc, data, mime} = req.body;
-        console.log(`Restoring asset to: ${loc} with mime: ${mime}`);
         AssetController.uploadToCloud(loc, data, mime).then(() => {
+            console.log(`Restoring asset to: ${loc}.`)
             res.status(200).send({message: `Uploaded asset to: ${loc}`});
         }).catch((e) => {
+            console.log(`Error uploading asset to: ${loc}. ${e.message}`);
             res.status(400).send({message: e.message});
         });
     }
